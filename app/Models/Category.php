497e61b4e213ac52026930846bb21b81663993d0
hary <haryarrahman14@gmail.com>
2024-10-19 20:09:23 +0700

    feat(admin) : category resource

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -16,4 +17,10 @@ class Category extends Model
         'tagline',
         'icon'
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
